f53b78ca5daea96a082789aa5a895fa7ca7f4b05
Christopher Chong <christopher.chong@Y22-Design-Christoper-Chong.local>
2018-09-25 11:45:58 -0400

    GUI process

--- CylinderCalculatorGUI.py
+++ CylinderCalculatorGUI.py
@@ -1,4 +1,26 @@
 import tkinter as tk 
+import math
+
+#Function
+def submit():
+
+	print("Submit pressed")
+	r = float(entr.get())
+	h = float(entr.get())
+
+	v = math.pi*r*r*h
+	v = round (v,3)
+
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
+	output.config(state="disabled")
+
+
+	output.insert(tk.INSERT,v)
+
+
+#Python programs find the first non indented code
+#to start.  and def is ignored until you call it
 
 root = tk.Tk()
 root.title("Volume of a Cyliner")
@@ -15,7 +37,7 @@ labh.pack()
 enth = tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text="submit")
+btn = tk.Button(root, text="submit", command=submit)
 btn.pack()
 
 output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
