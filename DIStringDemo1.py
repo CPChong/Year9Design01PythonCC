2167e999db0383a33aacbd3b723165dc1516423d
Christopher Chong <christopher.chong@Y22-Design-Christoper-Chong.local>
2018-11-13 20:33:54 -0500

    StringPracticeCode.py

---
+++ DIStringDemo1.py
@@ -0,0 +1,10 @@
+#Step 1 Take input foi first name
+fName = input("Input First Name: ")
+#Step 2 take input for last name
+lName = input("Input Last Name: ")
+
+#Process
+userName = fName[0] + lName 
+
+#ouput
+print(userName)
---
+++ StringPracticeCode.py
@@ -0,0 +1,5 @@
+define dad(str1):
+define mom(str2):
+define nainai(str3):
+	let str3 == str1 + str2 
+return str1 + str2
